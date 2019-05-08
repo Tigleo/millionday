eef77a3eef1856fab2c0a573be15787e93322957
Felice Simone <felice.simone@accenture.com>
2019-05-08 15:54:13 +0200

    rimosso script per requirements funziona analisi frequenza ambi e tris

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ print(verify_win(estrazioni[0], [5, 51, 23, 0, 0]))
 
 count_number = frequencies_counter(estrazioni)
 print("Count: {}".format(dict(count_number).items()))
+
+count_pair = frequencies_pairs(estrazioni)
+print("Pair Counter: {}".format(dict(count_pair).items()))
+
+count_tris = frequencies_triplets(estrazioni)
+print("Tris Counter: {}".format(dict(count_tris).items()))
--- utils/analisys.py
+++ utils/analisys.py
@@ -1,3 +1,4 @@
+import itertools
 from collections import OrderedDict
 from typing import List
 
@@ -17,6 +18,44 @@ def frequencies_counter(estrazioni: List[Estrazione]) -> OrderedDict:
             count += array.count(num)
         res[num - 1] = count
     frequencies = dict(zip(id, res))
+    return OrderedDict(sorted(frequencies.items(), key=lambda t: t[1], reverse=True))
+
+
+def frequencies_pairs(estrazioni: List[Estrazione]) -> OrderedDict:
+    base_num = list(range(1, 56))
+    pairs = list(itertools.combinations(base_num, 2))
+    res = np.zeros(len(pairs))
+    i = 0
+    for pair in pairs:
+        pair_count = 0
+        for es in estrazioni:
+            set_pair = set(pair)
+            set_es = set(es.numeri)
+            isSub = set_pair.issubset(set_es)
+            if (isSub):
+                pair_count += 1
+        res[i] = pair_count
+        i += 1
+    frequencies = dict(zip(pairs, res))
+    return OrderedDict(sorted(frequencies.items(), key=lambda t: t[1], reverse=True))
+
+
+def frequencies_triplets(estrazioni: List[Estrazione]) -> OrderedDict:
+    base_num = list(range(1, 56))
+    triplets = list(itertools.combinations(base_num, 3))
+    res = np.zeros(len(triplets))
+    i = 0
+    for tris in triplets:
+        tris_count = 0
+        for es in estrazioni:
+            set_pair = set(tris)
+            set_es = set(es.numeri)
+            isSub = set_pair.issubset(set_es)
+            if (isSub):
+                tris_count += 1
+        res[i] = tris_count
+        i += 1
+    frequencies = dict(zip(triplets, res))
     return OrderedDict(sorted(frequencies.items(), key=lambda t: t[1], reverse=True))
 
 
