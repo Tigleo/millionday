343d7b60348803eee9c18a3cce36d5b14056ab10
Felice Simone <felice.simone@accenture.com>
2019-06-25 14:13:02 +0200

    changed main

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from reader import *
 from scraper import *
+from upload import upload
 from utils.analisys import *
+from utils.json import *
 
 scrape(filename, datetime.now())
 read_file = make_filename(filename, datetime.now(), file_date_pattern)
 estrazioni = read(read_file)
 
-print(estrazioni[0])
-print(verify_win(estrazioni[0], [5, 51, 23, 0, 0]))
-
 count_number = frequencies_counter(estrazioni)
-print("Count: {}".format(dict(count_number).items()))
 
 count_pair = frequencies_pairs(estrazioni)
-print("Pair Counter: {}".format(dict(count_pair).items()))
 
-count_tris = frequencies_triplets(estrazioni)
-print("Tris Counter: {}".format(dict(count_tris).items()))
+tst = dict()
+result = dict()
+result["last_draw"] = last_draw_dict(estrazioni[0].data, estrazioni[0].numeri)
+result["frequenze"] = count_frequencies_dict(count_pair)
+tst["result"] = result
+
+json = json.dumps(tst)
+print(json)
+
+upload(tst)
